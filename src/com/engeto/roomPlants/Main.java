f976a3c1fcb606637b757ed1eae4e87c40ba4069
alceresna <alceresna@seznam.cz>
2022-08-16 16:43:41 +0200

    Creation of constant 'FILENAME'

--- src/com/engeto/roomPlants/Main.java
+++ src/com/engeto/roomPlants/Main.java
@@ -2,12 +2,14 @@
 package com.engeto.roomPlants;
 
 public class Main {
+
+    public static final String FILENAME = "kvetiny.txt";
     public static void main(String[] args) {
 
         ListOfPlants list;
 
         try {
-            list = ListOfPlants.importFromFile("kvetiny.txt");
+            list = ListOfPlants.importFromFile(FILENAME);
         } catch (PlantException e) {
             throw new RuntimeException(e);
         }
